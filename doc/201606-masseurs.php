75d92b251fa5ccf89d8f4d8258aa312eba30ffa1
mozodev <mozodev@users.noreply.github.com>
2016-06-13 13:25:30 +0900

    add detail page and link on front page

---
+++ doc/201606-masseurs.php
@@ -0,0 +1,22 @@
+<?
+$g4_path = "../gnu4";
+include_once($_SERVER['DOCUMENT_ROOT'].'/gnu4/common.php');
+
+//변수 정리
+  $g4['title'] = "작품 소개";
+  $LastModified = "2016-06-13 오후 2:00";
+  $is_dynamic = "False";
+  $Subject = "2016년 6월 마사지사 공연";
+  $Keywords = "Visual theater company, 꽃, 시각연극, 오브제극";
+
+// 상단 파일
+  include_once("head.php");
+?>
+
+<h1>2016년 6월 마사지사 공연</h1>
+
+<img src="/img/flower/201606-masseurs-large.jpg" alt="2016년 6월 마사지사 공연">
+
+<?
+include_once("tail.php");
+?>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include_once("_head.php");
   <div id="index-header">
     <div id="performance">
       <div id="current-performance">
-        <img src="/img/flower/banner-201606-large.jpg" alt="2016년 6월 마사지사 공연">
+        <a href="/doc/201606-masseurs.php"><img src="/img/flower/banner-201606-large.jpg" alt="2016년 6월 마사지사 공연"></a>
         <h2 class="text-center">2016년 6월 18, 19일 저녁 7시</h2>
         <h2 class="text-center"><a href="http://map.naver.com/local/siteview.nhn?code=11622409" target="_blank">대학로 마로니에 공원 (아르코 미술관 앞)</a></h2>
       </div>
